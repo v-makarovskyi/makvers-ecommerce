17da5eb664e8536986ac442f3cbba031d229d885
Vladimir <bigcorvet@gmail.com>
2023-09-03 15:33:41 +0300

    initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path')
 
-module.exports = nextConfig
+module.exports = {
+  webpack: {
+    module: {
+      loaders: [
+        {
+          test: /\.json$/,
+          loader: 'json-loader',
+        }
+      ]
+    }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*',
+      }
+    ]
+  }
+}
