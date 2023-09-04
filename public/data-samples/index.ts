2be28080414ba803a1a9258fe71c7eeb87918633
Vladimir <bigcorvet@gmail.com>
2023-09-04 20:08:40 +0300

    index

--- public/data-samples/index.ts
+++ public/data-samples/index.ts
@@ -1,4 +1,6 @@
 import navigationsLinks from './navigation.json';
+import productsData  from './products-data.json'
+import customersReviews from './customers-reviews.json'
 
+export { navigationsLinks,  productsData, customersReviews};
 
-export { navigationsLinks };
