55be94badd5ae7ba6e3b7babc899001efab95520
Vladimir <bigcorvet@gmail.com>
2023-09-04 16:07:34 +0300

    added classnames

--- components/small/buttons/SeeMoreButton.tsx
+++ components/small/buttons/SeeMoreButton.tsx
@@ -4,8 +4,8 @@ import styles from './Buttons.module.scss'
 
 export const SeeMoreButton: React.FC = () => {
     return (
-        <Link href='/about'>
-            <a>
+        <Link href='/about' legacyBehavior passHref>
+            <a className={styles.seeMore}>
                 <span>See More</span>
             </a>
         </Link>
